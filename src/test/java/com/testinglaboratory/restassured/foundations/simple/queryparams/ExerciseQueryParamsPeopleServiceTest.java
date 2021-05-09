44ea4ac3174f616057c269b15a9dcf59031a4dab
anetaaa <amajewska1@sii.pl>
2021-05-09 14:11:02 +0200

    rest

--- src/test/java/com/testinglaboratory/restassured/foundations/simple/queryparams/ExerciseQueryParamsPeopleServiceTest.java
+++ src/test/java/com/testinglaboratory/restassured/foundations/simple/queryparams/ExerciseQueryParamsPeopleServiceTest.java
@@ -37,8 +37,8 @@ public class ExerciseQueryParamsPeopleServiceTest {
 
     @Test
     public void shouldReturnListOfAllPeoplePaged() {
-       Response response = (Response) given() .queryParam("page_size", 5)
-              //  .queryParam("page_number", 2)
+        Response response = (Response) given().queryParam("page_size", 5)
+                //  .queryParam("page_number", 2)
                 .when()
 
                 .get("/get_all_people_paged")
@@ -47,11 +47,11 @@ public class ExerciseQueryParamsPeopleServiceTest {
                 .statusCode(HttpStatus.SC_OK)
                 .log().everything().extract();
     }
+
     @Test
     public void shouldReturnListOfPeopleByName() {
-        Response response = given()
-                .queryParam("first_name", "Mikołaj")
-               // .queryParam("last_name", "Rajtar")
+        Response response = given().queryParam("first_name", "Tomasz")
+                // .queryParam("last_name", "Rajtar")
                 .when()
                 .get("/get_people_by")
                 .then()
@@ -59,10 +59,37 @@ public class ExerciseQueryParamsPeopleServiceTest {
                 .statusCode(HttpStatus.SC_OK)
                 .log().everything().extract().response();
         List<Map<String, String>> people = response.getBody().jsonPath().getList(".");
-        for (Map<String, String> human: people) {
-            assertThat(human.get("first_name")).isEqualTo("Mikołaj");
+        for (Map<String, String> human : people) {
+            assertThat(human.get("first_name")).isEqualTo("Tomasz");
         }
     }
+
+    @Test
+    @DisplayName("No such person exists.")
+    public void shouldNotReturnListOfPeopleByName() {
+        Response response = given().queryParam("first_name", "Aneta")
+                // .queryParam("last_name", "Rajtar")
+                .when()
+                .get("/get_people_by")
+                .then()
+                .assertThat()
+                .statusCode(HttpStatus.SC_NOT_FOUND)
+                .log().everything().extract().response();
+
+        response.then().statusCode(22);
+        response.body().jsonPath().getString("message");
+
+        response.jsonPath().getString("message");
+        response.xmlPath();
+        response.htmlPath();
+
+
+        List<Map<String, String>> people = response.getBody().jsonPath().getList(".");
+        for (Map<String, String> human : people) {
+            assertThat(human.get("first_name")).isEqualTo("Aneta");
+        }
+    }
+
     @DisplayName("/get_people_by without params")
     @Test
     public void shouldFailedReturnListOfPeopleByName() {
